bb7c2b2b155bb3eb428f9b5b2a0d113022ea8c43
Burgerface627 <burgerface627@outlook.com>
2022-11-18 16:05:16 -0500

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='EasyCodrone-EduCommands',
     version='0.0.2',
     packages=['easycodronecommands'],
-    url='',
+    url='https://github.com/burgerface627/EasyCodroneEduCommands',
     license='MIT License  Copyright (c) [2022] [Alex Birdsall]  Permission is hereby granted, free of charge, '
             'to any person obtaining a copy of this software and associated documentation files (the "Software"), '
             'to deal in the Software without restriction, including without limitation the rights to use, copy, '
