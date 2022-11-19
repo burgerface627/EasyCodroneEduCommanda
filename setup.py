b737dab53dc9c82ef3028008f237fa10f409dfec
Burgerface627 <burgerface627@outlook.com>
2022-11-19 15:05:40 -0500

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='EasyCodrone-EduCommands',
-    version='0.0.5',
+    version='0.0.6',
     packages=['easycodronecommands'],
     url='https://github.com/burgerface627/EasyCodroneEduCommands',
     license='MIT',
